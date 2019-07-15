9a775118ee8357d898cc24e405ce463c35f9550d
Kevin  Dicke <kdicke@klosch-7250.guess.net>
2019-07-15 16:02:35 -0700

    inital commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+function show(){
+		var pxValue = jQuery('#pixel').val();
+		var vpwidth = jQuery('#vpwidth').val();
+		var vwValue = String(parseFloat(pxValue / vpwidth) * 100).split(".");
+    vwValue[1] = vwValue[1].substring(0,2)
+		vwValue = vwValue.join(".")
+    $('.output label').text(vwValue + "vw");
+		$('.output').show();
+}
+function copyToClipboard(element) {
+  $("button").hide()
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+  $("#copied").show()
+  $("#copied").fadeOut(1500)
+}
